4ac35b1dcfa0831121d56b257c570d4ed2b571b3
Hans <hhanckes@gmail.com>
2014-08-12 17:21:40 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vapstick::Application.config.secret_key_base = '61fe4462955c82f5891173f07e1a2c1a3e102823f6aadcd86376ef9ec2817523b3e6ef910dd35b5a9110b808c357cf89fcfbc7a553eae958dedfe7fbcf0ffeab'
